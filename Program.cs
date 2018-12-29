018ac0270c8f180e45272533e10ea011d0129cd9
jmorg85 <justicemorgan64@yahoo.com>
2018-12-28 20:35:52 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main()
+        {
+        bool? areyoumajor = null;
+        string FirstName;
+        string LastName;
+        string answer;
+        Console.WriteLine("What is your first name?");
+        FirstName = Console.ReadLine();
+        Console.WriteLine("What is your last name?");
+        LastName = Console.ReadLine();
+        Console.WriteLine("Are you major? Yes or No.");
+        answer = Console.ReadLine();
+        Console.WriteLine(FirstName + "\n" + LastName + "\n");
+
+        if (answer == "Yes")
+        {
+            areyoumajor = true;
+        }
+
+        if (answer == "No")
+        {
+            areyoumajor = false;
+        }
+
+        if (areyoumajor == true)
+        {
+            Console.WriteLine("you are major.");
+        }
+
+        else if(areyoumajor == false)
+        {
+            Console.WriteLine("You're not major.");
+        }
+        else
+        {
+            Console.WriteLine("There was no answer.");
+        }
+
+    }
+    }
